9ee3b077648292cfe7d586c4e85bba050e17688b
Laurenz Honauer <laurenz.honauer@gmail.com>
2022-05-22 11:08:44 +0200

    Revert expo setup

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,28 +1,3 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      ["@babel/plugin-transform-flow-strip-types"],
-      ["@babel/plugin-proposal-decorators", { "legacy": true }],
-      ["@babel/plugin-proposal-class-properties", { "loose": true }],
-      ["module-resolver", {
-        "alias": {
-          "@Navigation": "./navigation",
-          "@Components": "./components",
-          "@Screens": "./screens",
-          "@Assets": "./assets",
-          "@Hooks": "./hooks",
-          "@Constants": "./constants",
-          "@Types": ".types",
-        },
-        "extensions": [
-          ".js",
-          ".jsx",
-          ".ts",
-          ".tsx",
-        ]
-      }],
-    ]
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
 };
