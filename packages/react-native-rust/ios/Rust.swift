5abb519488b67d12438f3447d262a2b14c991189
Laurenz Honauer <laurenz.honauer@gmail.com>
2022-05-26 12:31:45 +0200

    Add new react module boilerplate

---
+++ packages/react-native-rust/ios/Rust.swift
@@ -0,0 +1,8 @@
+@objc(Rust)
+class Rust: NSObject {
+
+    @objc(multiply:withB:withResolver:withRejecter:)
+    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve(a*b)
+    }
+}
