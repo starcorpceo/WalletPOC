17c38902ea4587c47b5e73a894ae07ba93305806
Mathias Nahringbauer <mathias.nahringbauer@gmail.com>
2022-06-15 11:55:45 +0200

    added share part to initGenerateEcdsaKey

--- packages/react-native-blockchain-crypto-mpc/ios/BlockchainCryptoMpc.mm
+++ packages/react-native-blockchain-crypto-mpc/ios/BlockchainCryptoMpc.mm
@@ -15,6 +15,7 @@ RCT_REMAP_METHOD(initGenerateEcdsaKey,
     int rv = 0;
     
     MPCCryptoContext *context;
+    MPCCryptoShare *share = nullptr;
     
     if((rv = MPCCrypto_initGenerateEcdsaKey(1, &context))) {
         resolve(@(&"Failure " [ rv ]));
@@ -23,8 +24,36 @@ RCT_REMAP_METHOD(initGenerateEcdsaKey,
     unsigned flags = 0;
 
     MPCCryptoMessage *message = nullptr;
+    
     if((rv = MPCCrypto_step(context, nullptr, &message, &flags))){
         resolve(@(&"Failure " [ rv ]));
+    }
+    
+    if((flags & mpc_share_changed)){
+        
+        MPCCrypto_freeShare(share);
+        share = nullptr;
+        if ((rv = MPCCrypto_getShare(context, &share))){
+            resolve(@(&"Failure " [ rv ]));
+        }
+        std::vector<uint8_t> share_buf;
+        
+        //instead of share_to_buf() from tests
+        int size = 0;
+        if ((rv = MPCCrypto_shareToBuf(share, nullptr, &size))){
+            resolve(@(&"Failure " [ rv ]));
+        }
+        share_buf.resize(size);
+        if ((rv = MPCCrypto_shareToBuf(share, share_buf.data(), &size))){
+            resolve(@(&"Failure " [ rv ]));
+        }
+        
+        MPCCrypto_freeShare(share);
+        
+        share = nullptr;
+        if ((rv = MPCCrypto_shareFromBuf(share_buf.data(), (int)share_buf.size(), &share))){
+            resolve(@(&"Failure " [ rv ]));
+        }
     }
     
     int size = 0;
@@ -32,29 +61,27 @@ RCT_REMAP_METHOD(initGenerateEcdsaKey,
     if((rv = MPCCrypto_messageToBuf(message, nullptr, &size))) {
         resolve(@(&"Failure " [ rv ]));
     }
-    
     message_buf.resize(size);
-    
     if((rv = MPCCrypto_messageToBuf(message, message_buf.data(), &size))){
         resolve(@(&"Failure " [ rv ]));
     }
     
-    char messageString[size+1];
+     
     
-    memcpy(messageString, message_buf.data(), size);
-    
-    //NSString *messageString = ([NSString stringWithFormat:@"%@", message]);
-    //std::string foo = std::string([messageString UTF8String]);
-    
-    NSString *aasdf = [NSString stringWithCString:messageString encoding:NSASCIIStringEncoding];
+    char bufferString[size+1];
+    memcpy(bufferString, message_buf.data(), size);
+    bufferString[size] = '\0';
+        
+    NSString *bufferNSString = [NSString stringWithCString:bufferString encoding:[NSString defaultCStringEncoding]];
 
-    
+        
     if(rv == 0) {
-        resolve(@{@"res": aasdf});
+        resolve(bufferNSString);
     } else {
         resolve(@(&"Failure " [ rv ]));
     }
 
 }
+
 
 @end
