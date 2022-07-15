7f5e4743a1de1918a3741ed3764a07cc65b6a393
Laurenz Honauer <laurenz.honauer@gmail.com>
2022-07-15 13:34:01 +0200

    Remove test mpc screen

--- mobile/app/views/test-mpc/style.ts
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from "react-native";
-
-export const groupStyle = {
-  borderBottomColor: "black",
-  borderBottomWidth: StyleSheet.hairlineWidth,
-  paddingBottom: 10,
-  marginBottom: 10,
-};
